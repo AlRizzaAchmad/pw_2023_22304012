dbb5a40c969b5faa24b09ccfc6ba9fb1c51dff29
AlRizzaAchmad <126079298+AlRizzaAchmad@users.noreply.github.com>
2023-02-22 22:46:16 +0700

    Add files via upload

---
+++ tugas/PHP/A1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A1</title>
+</head>
+<body>
+    <?php 
+    $kata1 = "Topi" ;
+    $kata2 = "Bundar" ;
+    ?>
+<h1>
+    <i><b> "<?php echo $kata1 ." saya ". $kata2 ." , " . $kata2 . " " . $kata1 .' saya" '  ;
+    ?></i>
+    </b>
+    </h1>
+</body>
+</html>
---
+++ tugas/PHP/B1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>B1</title>
+</head>
+<body>
+    <?php
+    $NRP = 23; 
+     ?>
+     <?php echo "Aku Adalah Angka" . "<b>" . $NRP . "</b>"  ?>
+     <br>
+     <?php echo "Jika aku dikali 5, maka aku sekarang menjadi " . "<b>" . 5*$NRP . "</b>" ?>
+     <br>
+     <?php echo "Jika aku dibagi 2, maka aku sekarang menjadi " . "<b>" . 5*$NRP/2 . "</b>"  ?>
+     <br>
+     <?php echo "Jika aku ditambah 75, maka aku sekarang menjadi " . "<b>" . 5*$NRP/2+75 . "</b>" ?>
+     <br>
+     <?php echo "Jika aku dikurang 20, maka aku sekarang menjadi " . "<b>" . 5*$NRP/2+75-20 . "</b>"  ?>
+     
+</body>
+</html>
